8d4185c65e19493e22283292262d1a0ec7d98160
Tony van Loon <34678523+3Dimensions-Tony@users.noreply.github.com>
2024-03-12 11:57:13 +0100

    Update GameObjectSteps.cs

--- Runtime/Scripts/GameObjectSteps.cs
+++ Runtime/Scripts/GameObjectSteps.cs
@@ -1,6 +1,6 @@
 using Sirenix.OdinInspector;
 using UnityEngine;
-namespace _3Dimensions.Runtime.Scripts
+namespace _3Dimensions.Tools.Runtime.Scripts
 {
     public class GameObjectSteps : MonoBehaviour
     {
